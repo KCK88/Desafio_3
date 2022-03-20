ff739e49fe5bfe6eee9ebac633d14163211a6083
Claudio Meira <claudiomeirabs@gmail.com>
2022-03-20 00:43:30 -0300

    Desafio feito

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Desafio_02
 {
@@ -30,16 +31,36 @@ namespace Desafio_02
 
 
             //T1 - Somar a idade de todos Ubuntus
-
+            int somaIdade = idade1+idade2+idade3;
+            Console.WriteLine(somaIdade);
             //T2 - Agrupar os nomes de todos Ubuntus
-
+            List<string> nomesUbuntus = new List<string>();
+            nomesUbuntus.Add(nome1);
+            nomesUbuntus.Add(nome2);
+            nomesUbuntus.Add(nome3);
+            foreach (string nome in nomesUbuntus)
+                {
+                    Console.WriteLine(nome);
+                }
             //T3 - Calcular Média dos IMC de todos Ubuntus
-
+            double imc1 = peso1/(double)(altura1*altura1);
+            double imc2 = peso2/(double)(altura2*altura2);
+            double imc3 = peso3/(double)(altura3*altura3);
+            double imcTotal = (imc1+imc2+imc3)/3;
+            Console.WriteLine(imcTotal);
             //T4 - Contar quantos Ubuntus sao Devs
-
+            int totalDevs = 0;
+            if (EhDev1 == true)
+                totalDevs++;
+            if (EhDev2)
+                totalDevs = totalDevs + 1;
+            totalDevs+= EhDev3? 1 : 0;     
+            Console.WriteLine(totalDevs);
+            
             //T5 - Exibir somente Ubuntu com nome Silva
-
-
+            PrintUbuntus(nome1);
+            PrintUbuntus(nome2);
+            PrintUbuntus(nome3);
 
             //Imprime na tela
             Console.WriteLine("UBUNTU 002 - ENDS...");
@@ -47,7 +68,11 @@ namespace Desafio_02
             //PAUSA NA TELA (GAMBIARRA PERMITIDA PRA INICIANTES)
             Console.Read();
         }
-
+        static void PrintUbuntus(string nome)
+        {
+            if(nome.IndexOf("Silva")!= -1)
+                Console.WriteLine(nome);
+        }
 
     }
 }
